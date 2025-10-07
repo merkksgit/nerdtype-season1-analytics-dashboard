d47a7669316f58feff765100f69c06abaea4d1e4
merkksgit <merkks@protonmail.com>
2025-10-07 22:30:44 +0300

    modified:   convert.py

--- convert.py
+++ convert.py
@@ -7,3 +7,11 @@ df = pd.read_json("scores_data.json")
 df.to_excel("scores_data.xlsx", index=False)
 
 print("JSON file successfully converted to XLSX!")
+
+# Export to CSV
+
+# df = pd.read_json('scores_data.json')
+#
+# df.to_csv('scores_data.csv', index=False)
+#
+# print("JSON file successfully converted to CSV!")
