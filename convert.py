e3878b431d109f1db51f3bc33e01b5f043f35976
merkksgit <merkks@protonmail.com>
2025-10-07 22:33:55 +0300

    modified:   convert.py

--- convert.py
+++ convert.py
@@ -1,17 +1,11 @@
 import pandas as pd
 
-# Read the JSON file
 df = pd.read_json("scores_data.json")
-
-# Export to Excel format
 df.to_excel("scores_data.xlsx", index=False)
-
 print("JSON file successfully converted to XLSX!")
 
 # Export to CSV
 
 # df = pd.read_json('scores_data.json')
-#
 # df.to_csv('scores_data.csv', index=False)
-#
 # print("JSON file successfully converted to CSV!")
